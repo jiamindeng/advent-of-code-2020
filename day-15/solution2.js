a0f9b909ff67d443c497153ece8aba2dd719e9bc
Jiamin Deng <jam@temple.edu>
2020-12-18 22:52:28 -0500

    Redo day-16, Part Two

--- day-15/solution2.js
+++ day-15/solution2.js
@@ -5,7 +5,7 @@ let input = fs
   .split(/,/)
   .map((num) => parseInt(num));
 
-let turns = {};
+let turns = new Map();
 
 let iterator = input.length;
 let lastNum = input[input.length - 1];
@@ -13,23 +13,27 @@ let lastNum = input[input.length - 1];
 let start = new Date().getTime();
 
 input.forEach((num, index) => {
-  Object.assign(turns, { [num]: { recent: index, lastSeen: -1 } });
+  turns.set(num, [index, -1]);
+  //Object.assign(turns, { [num]: { recent: index, lastSeen: -1 } });
 });
 
 while (iterator < 30000000) {
-  let recent = turns[lastNum].recent;
-  let lastSeen = turns[lastNum].lastSeen;
+  let recent = turns.get(lastNum)[0];
+  let lastSeen = turns.get(lastNum)[1];
+  // let recent = turns[lastNum].recent;
+  // let lastSeen = turns[lastNum].lastSeen;
 
   if (lastSeen === -1) {
     lastNum = 0;
-    turns[0] = { recent: iterator, lastSeen: turns[0] ? turns[0].recent : -1 };
+    turns.set(0, [iterator, turns.get(0) ? turns.get(0)[0] : -1]);
+    //  turns[0] = { recent: iterator, lastSeen: turns[0] ? turns[0].recent : -1 };
   } else {
     let difference = recent - lastSeen;
     lastNum = difference;
-    turns[difference] = {
-      recent: iterator,
-      lastSeen: turns[difference] ? turns[difference].recent : -1,
-    };
+    turns.set(difference, [
+      iterator,
+      turns.get(difference) ? turns.get(difference)[0] : -1,
+    ]);
   }
   iterator++;
 }
