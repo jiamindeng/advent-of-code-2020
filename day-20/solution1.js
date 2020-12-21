81fb88f83a8b41354e357fa5ff1021750b4785fe
Jiamin Deng <jam@temple.edu>
2020-12-20 19:37:24 -0500

    Day Twenty, Part Two

--- day-20/solution1.js
+++ day-20/solution1.js
@@ -2,21 +2,25 @@ const fs = require('fs');
 
 let input = fs.readFileSync('./input.txt', 'utf-8').split(/\r\n\r\n/);
 
-const tiles = input.map((string) => {
-  const lines = string.split(/\r\n/);
-  const tile = {
+let tiles = input.map((string) => {
+  let lines = string.split(/\r\n/);
+  let tile = {
     id: lines[0].replace(/Tile /, '').replace(/:/, ''),
-    top: lines[1],
-    bottom: lines[lines.length - 1],
-    left: lines
-      .slice(1)
-      .map((line) => line[0])
-      .join(''),
-    right: lines
-      .slice(1)
-      .map((line) => line[line.length - 1])
-      .join(''),
-    neighbors: 0,
+    sides: {
+      top: lines[1],
+      bottom: lines[lines.length - 1],
+      left: lines
+        .slice(1)
+        .map((line) => line[0])
+        .join(''),
+      right: lines
+        .slice(1)
+        .map((line) => line[line.length - 1])
+        .join(''),
+    },
+    body: lines.slice(1),
+    neighborTiles: new Set(),
+    sharedSides: new Set(),
   };
   return tile;
 });
@@ -24,28 +28,28 @@ const tiles = input.map((string) => {
 const labelAdjacentTiles = (currentTile) => {
   for (tile of tiles) {
     if (tile.id !== currentTile.id) {
-      for (currentProp in currentTile) {
-        for (prop in tile) {
+      for (currentProp in currentTile.sides) {
+        for (prop in tile.sides) {
           if (
             prop !== 'neighbors' &&
             currentProp !== 'neighbors' &&
-            currentTile[currentProp] === tile[prop]
+            currentTile.sides[currentProp] === tile.sides[prop]
           ) {
-            console.log(currentTile.id, tile.id, { currentProp, prop });
-            currentTile[currentProp] = `${tile.id}:${prop}`;
-            tile[prop] = `${currentTile.id}:${currentProp}`;
-            currentTile.neighbors++;
-            tile.neighbors++;
+            currentTile.neighborTiles.add(tile.id);
+            tile.neighborTiles.add(currentTile.id);
+            currentTile.sharedSides.add(currentProp);
+            tile.sharedSides.add(prop);
           } else if (
             prop !== 'neighbors' &&
             currentProp !== 'neighbors' &&
-            currentTile[currentProp] === tile[prop].split('').reverse().join('')
+            !Array.isArray(tile.sides[prop]) &&
+            currentTile.sides[currentProp] ===
+              tile.sides[prop].split('').reverse().join('')
           ) {
-            console.log(currentTile.id, tile.id, { currentProp, prop });
-            currentTile[currentProp] = `${tile.id}:${prop}:reversed`;
-            tile[prop] = `${currentTile.id}:${currentProp}:reversed`;
-            currentTile.neighbors++;
-            tile.neighbors++;
+            currentTile.neighborTiles.add(tile.id);
+            tile.neighborTiles.add(currentTile.id);
+            currentTile.sharedSides.add(currentProp);
+            tile.sharedSides.add(prop);
           }
         }
       }
@@ -56,8 +60,8 @@ const labelAdjacentTiles = (currentTile) => {
 tiles.forEach((tile) => labelAdjacentTiles(tile));
 
 let corners = tiles
-  .filter((tile) => tile.neighbors === 2)
+  .filter((tile) => tile.neighborTiles.size === 2)
   .map((tile) => parseInt(tile.id))
   .reduce((acc, current) => acc * current);
 
-console.log(tiles.map((tile) => tile.neighbors));
+console.log(`Part One: ${corners}`);
--- day-20/solution2.js
+++ day-20/solution2.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 let input = fs.readFileSync('./input.txt', 'utf-8').split(/\r\n\r\n/);
 
+// Nope
+
 let tiles = input.map((string) => {
   let lines = string.split(/\r\n/);
   let tile = {
@@ -19,16 +21,20 @@ let tiles = input.map((string) => {
         .join(''),
     },
     body: lines.slice(1),
-    neighborTiles: [],
+    neighborTiles: new Set(),
+    sharedSides: new Set(),
   };
   return tile;
 });
 
+let puzzleSideLength = Math.sqrt(tiles.length);
+
 const flipTile = (tile) => {
   tile.body = tile.body.reverse();
-  [tile.sides.top, tile.sides.bottom] = [tile.sides.bottom, tile.sides.top];
-  tile.sides.left = tile.sides.left.split('').reverse().join('');
-  tile.sides.right = tile.sides.right.split('').reverse().join('');
+  tile.sides.top = tile.body[0];
+  tile.sides.bottom = tile.body[tile.body.length - 1];
+  tile.sides.left = tile.body.map((line) => line[0]).join('');
+  tile.sides.right = tile.body.map((line) => line[line.length - 1]).join('');
 };
 
 const rotateTile = (tile) => {
@@ -38,11 +44,10 @@ const rotateTile = (tile) => {
   );
 
   tile.body = bodyArray.reverse().map((line) => line.join(''));
-  let tmp = tile.sides.top;
-  tile.sides.top = tile.sides.right;
-  tile.sides.right = tile.sides.bottom;
-  tile.sides.bottom = tile.sides.left;
-  tile.sides.left = tmp;
+  tile.sides.top = tile.body[0];
+  tile.sides.bottom = tile.body[tile.body.length - 1];
+  tile.sides.left = tile.body.map((line) => line[0]).join('');
+  tile.sides.right = tile.body.map((line) => line[line.length - 1]).join('');
 };
 
 const isCorrectPair = (currentProp, prop) => {
@@ -68,7 +73,6 @@ const getConfigurations = (tile) => {
       configurations.push(JSON.parse(JSON.stringify(tileCopy)));
     }
   }
-
   return configurations;
 };
 
@@ -82,20 +86,21 @@ const labelAdjacentTiles = (currentTile) => {
             currentProp !== 'neighbors' &&
             currentTile.sides[currentProp] === tile.sides[prop]
           ) {
-            currentTile.sides[currentProp] = `${tile.id}:${prop}`;
-            tile.sides[prop] = `${currentTile.id}:${currentProp}`;
-            currentTile.neighborTiles.push(tile.id);
-            tile.neighborTiles.push(currentTile.id);
+            currentTile.neighborTiles.add(tile.id);
+            tile.neighborTiles.add(currentTile.id);
+            currentTile.sharedSides.add(currentProp);
+            tile.sharedSides.add(prop);
           } else if (
             prop !== 'neighbors' &&
             currentProp !== 'neighbors' &&
+            !Array.isArray(tile.sides[prop]) &&
             currentTile.sides[currentProp] ===
               tile.sides[prop].split('').reverse().join('')
           ) {
-            currentTile.sides[currentProp] = `${tile.id}:${prop}:reversed`;
-            tile.sides[prop] = `${currentTile.id}:${currentProp}:reversed`;
-            currentTile.neighborTiles.push(tile.id);
-            tile.neighborTiles.push(currentTile.id);
+            currentTile.neighborTiles.add(tile.id);
+            tile.neighborTiles.add(currentTile.id);
+            currentTile.sharedSides.add(currentProp);
+            tile.sharedSides.add(prop);
           }
         }
       }
@@ -103,22 +108,170 @@ const labelAdjacentTiles = (currentTile) => {
   }
 };
 
+let puzzle = Array.from({ length: puzzleSideLength * 2 }, () =>
+  Array.from({ length: puzzleSideLength * 2 }, () => 0)
+);
+
+tiles.forEach((tile) => labelAdjacentTiles(tile));
+
+let corners = tiles.filter((tile) => tile.neighborTiles.size === 2);
+
 let configurations = {};
 tiles.forEach((tile) =>
   Object.assign(configurations, { [tile.id]: getConfigurations(tile) })
 );
 
-let all = new Set(tiles.map((tile) => tile.id));
-let puzzle = Array.from({ length: Math.sqrt(tiles.length) }, () =>
-  Array.from({ length: Math.sqrt(tiles.length) }, () => 0)
+let start = configurations[corners[0].id][0];
+
+puzzle[puzzleSideLength][puzzleSideLength] = start;
+
+const putSides = () => {
+  puzzle.forEach((row, rowIndex) =>
+    row.forEach((tile, tileIndex) => {
+      if (tile !== 0) {
+        Object.entries(tile.sides).forEach(([tileProp, side]) => {
+          for (id in configurations) {
+            if (id !== tile.id) {
+              configurations[id].forEach((configuration) => {
+                for (prop in configuration.sides) {
+                  if (
+                    isCorrectPair(tileProp, prop) &&
+                    side === configuration.sides[prop]
+                  ) {
+                    switch (tileProp) {
+                      case 'top':
+                        puzzle[rowIndex - 1][tileIndex] = configuration;
+                        break;
+                      case 'bottom':
+                        puzzle[rowIndex + 1][tileIndex] = configuration;
+                        break;
+                      case 'left':
+                        puzzle[rowIndex][tileIndex - 1] = configuration;
+                        break;
+                      case 'right':
+                        puzzle[rowIndex][tileIndex + 1] = configuration;
+                        break;
+                    }
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    })
+  );
+};
+
+let count = tiles.length;
+
+while (count > 0) {
+  putSides();
+  count--;
+}
+
+const constrain = (puzzle) => {
+  return puzzle
+    .map((row) => row.filter((tile) => tile))
+    .filter((row) => row.length);
+};
+
+let finishedPuzzle = constrain(puzzle).map((row) =>
+  row.map((tile) => tile.body)
 );
-tiles.forEach((tile) => labelAdjacentTiles(tile));
 
-let corners = tiles.filter((tile) => tile.neighborTiles.length === 2);
+let mergedPuzzle = [];
 
-let start = configurations[corners[0].id].filter((tile) => {
-  let regExp = /[a-zA-Z]/g;
-  return regExp.test(tile.sides.right) && regExp.test(tile.sides.bottom);
-})[0];
+finishedPuzzle.forEach((horizontalChunk) => {
+  for (let i = 1; i < horizontalChunk[0].length - 1; i++) {
+    let mergedPuzzleRow = '';
+    for (let j = 0; j < puzzleSideLength; j++) {
+      mergedPuzzleRow += horizontalChunk[j][i].slice(
+        1,
+        horizontalChunk[j][i].length - 1
+      );
+    }
+    mergedPuzzle.push(mergedPuzzleRow);
+  }
+});
 
-puzzle[0][0] = start;
+const getPuzzleConfigurations = (puzzle) => {
+  const flip = (puzzle) => {
+    return puzzle.reverse();
+  };
+
+  const rotate = (puzzle, n) => {
+    let puzzleCopy = puzzle.map((line) => line.split(''));
+    puzzleCopy = puzzleCopy[0].map((_, colIndex) =>
+      puzzleCopy.map((row) => row[colIndex])
+    );
+    return puzzleCopy.reverse().map((line) => line.join(''));
+  };
+
+  const configurations = [];
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 4; j++) {
+      let puzzleCopy = JSON.parse(JSON.stringify(puzzle));
+      if (i) {
+        puzzleCopy = flip(puzzleCopy);
+      }
+      for (let k = 0; k < j; k++) {
+        puzzleCopy = rotate(puzzleCopy);
+      }
+
+      configurations.push(puzzleCopy);
+    }
+  }
+  return configurations;
+};
+
+let puzzleConfigurations = getPuzzleConfigurations(mergedPuzzle);
+
+const findNessie = (puzzle) => {
+  let nessieInput = fs.readFileSync('./nessie.txt', 'utf-8');
+  let nessie = nessieInput.split(/\r\n/).map((line) => line.split(''));
+
+  let nessieCoordinates = nessie
+    .map((line, lineIndex) =>
+      line
+        .map((char, charIndex) => (char === '#' ? [lineIndex, charIndex] : 0))
+        .filter((char) => char)
+    )
+    .flat();
+
+  let puzzleArray = puzzle.map((line) => line.split(''));
+  for (let i = 0; i < puzzleArray.length; i++) {
+    for (let j = 0; j < puzzleArray[0].length; j++) {
+      let isNessie = true;
+      let nessieFound = [];
+      outer: for ([dy, dx] of nessieCoordinates) {
+        let y = i + dy;
+        let x = j + dx;
+        if (y < puzzleArray.length && x < puzzleArray.length) {
+          if (puzzleArray[y][x] !== '#') {
+            isNessie = false;
+            nessieFound = [];
+            break outer;
+          } else {
+            nessieFound.push([y, x]);
+          }
+        } else {
+          nessieFound = [];
+          break outer;
+        }
+      }
+      if (isNessie) {
+        nessieFound.forEach(([y, x]) => (puzzleArray[y][x] = 0));
+      }
+    }
+  }
+  return puzzleArray.map((line) => line.join('')).join('\n');
+};
+
+let search = puzzleConfigurations
+  .map((puzzleConfiguration) => findNessie(puzzleConfiguration))
+  .filter((puzzleConfiguration) => puzzleConfiguration.includes(0));
+
+[correctPuzzle] = search;
+
+console.log(`Part Two: ${correctPuzzle.match(/#/g).length}`);
